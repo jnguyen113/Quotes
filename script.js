4152fa923bb90f396c6eb4bd5af223950e121ea1
Johnathan <johnathan.nguyen1999@gmail.com>
2020-07-30 17:08:44 -0400

    Framework set up
    Started building the framework/front-end of this project

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+//button to show form-popup
+function toggleForm() {
+    var form = document.getElementById("myForm");
+    //gets the current value of display (none)
+    var displaySetting = form.style.display;
+    var formButton = document.getElementById("newQuote");
+  
+    //if form currently shows, hide it when button clicked
+    if (displaySetting == "block") {
+      //form visible hide it
+      form.style.display = "none";
+      formButton.innerText = "Add Book";
+    } else {
+      form.style.display = "block";
+      formButton.innerText = "Hide Me";
+    }
+  }
