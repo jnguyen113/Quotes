c12096a9a721fd886c4aab0893d4e858e5b0d339
Johnathan <johnathan.nguyen1999@gmail.com>
2020-08-10 09:10:36 -0400

    added local storage
    working local storage

--- script.js
+++ script.js
@@ -1,43 +1,95 @@
-let quotes = [];//empty array to store all the quotes in 
+//getting local storage function
+function getQuotes() {
+  var quotes = new Array;
+  //the item became a string, so we need to parse it
+  var stringedQuotes = localStorage.getItem('quote');
+  //as long as there are items in local storage
+  if (stringedQuotes !== null) {
+      quotes = JSON.parse(stringedQuotes); 
+  }
+  return quotes;
+}
+
+ //<button type="button" class="add" id="add">+</button>
+//whenever you are adding, you set the item in local storage
+function add() {
+  var bquotes = document.getElementById("quote").value;
+  var author = document.getElementById("author").value;
+  var chapter = document.getElementById("chapter").value;
+  var verses = document.getElementById("verses").value;
+
+  var quotes = getQuotes();
+  quotes.push(bquotes, author, chapter, verses);
+  localStorage.setItem('quote', JSON.stringify(quotes));
+  show();
+
+
+  var clearVerses = (document.getElementById("verses").value = "");
+  var clearQuotes = (document.getElementById("quote").value = "");
+  var clearAuthor = (document.getElementById("author").value = "");
+  var clearChapter = (document.getElementById("chapter").value = "");
+  return false;
+}
 
 //these have to be the same as the bottom declarations
-function bibleQuote(quotes, author, chapter, verses){
-  this.quotes = quotes;
-  this.author = author;
-  this.chapter = chapter;
-  this.verses = verses;
+// function bibleQuote(quotes, author, chapter, verses){
+//   this.quotes = quotes;
+//   this.author = author;
+//   this.chapter = chapter;
+//   this.verses = verses;
+// }
+
+
+// const add = document.getElementById("addQuote");
+// add.addEventListener('click', function (){
+
+//   let quotes = document.getElementById("quote").value;
+//   var clearQuotes = (document.getElementById("quote").value = "");
+
+//   let author = document.getElementById("author").value;
+//   var clearAuthor = (document.getElementById("author").value = "");
+
+//   let chapter = document.getElementById("chapter").value;
+//   var clearChapter = (document.getElementById("chapter").value = "");
+
+//   let verses = document.getElementById("verses").value;
+//   var clearVerses = (document.getElementById("verses").value = "");
+
+//   // //creating a new object of above information
+//   let quote = new bibleQuote(quotes, author, chapter, verses);
+//   quote.addToLibrary();
+
+//   //creating element list
+//   var div = document.createElement("div");
+//   div.classList.add("listOfQuotes");
+//   div.innerText = `" ${quote.quotes}"
+//   - ${quote.author} ${quote.chapter}: ${quote.verses}`;
+
+//   var remove = document.createElement("button");
+//   remove.classList.add("remove");
+//   remove.innerText = "x";
+//   remove.addEventListener('click', function(){
+
+//   });
+  
+//   document.getElementById("library").append(div, remove);
+//   //document.getElementById("library").append(remove);
+// });
+
+function remove() {
+  //gets the specific id of element
+  var id = this.getAttribute('id');
+  //calling function todo to get items
+  var quotes = getQuotes();
+  //splice that specific id once
+  quotes.splice(id, 1);
+  //after splicing, set up new todos again
+  localStorage.setItem('quote', JSON.stringify(quotes));
+  //show the change
+  show();
+  return false;
 }
 
-bibleQuote.prototype.addToLibrary = function (){
-  quotes.push(this);//empty array pushing quote into array
-  return this;
-}
-
-const add = document.getElementById("addQuote");
-add.addEventListener('click', function (){
-
-  let quotes = document.getElementById("quote").value;
-  var clearQuotes = (document.getElementById("quote").value = "");
-
-  let author = document.getElementById("author").value;
-  var clearAuthor = (document.getElementById("author").value = "");
-
-  let chapter = document.getElementById("chapter").value;
-  var clearChapter = (document.getElementById("chapter").value = "");
-
-  let verses = document.getElementById("verses").value;
-  var clearVerses = (document.getElementById("verses").value = "");
-
-  // //creating a new object of above information
-  let quote = new bibleQuote(quotes, author, chapter, verses);
-  quote.addToLibrary();
-
-  //creating element list
-  var li = document.createElement("li");
-  li.innerText = `Quote: ${quote.quotes}, Author: ${quote.author}, Chapter: ${quote.chapter}, Verses: ${quote.verses}`;
-
-  document.getElementById("library").append(li);
-});
 
 //button to show form-popup
 function toggleForm() {
@@ -56,3 +108,27 @@ function toggleForm() {
       formButton.innerText = "Hide Me";
     }
   }
+
+  function show() {
+    var quotes = getQuotes();
+    //start with a list
+    var html = '<ul>';
+    //creating list per length and remove button
+    for(var i=0; i<quotes.length; i++) {
+        html += '<li>' + quotes[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+      
+      };
+    html += '</ul>'; //end with unordered list
+    
+    document.getElementById('list').innerHTML = html;
+ 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length / 4; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+
+   
+}
+
+document.getElementById('addQuote').addEventListener('click', add);
+show();
